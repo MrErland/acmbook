f6e3b206bbad8acbb82936a63c078eae06c58984
Erland <912859459@qq.com>
2018-01-23 16:22:26 +0800

    修改了VIM下P4_2.cpp粘贴格式错乱的问题。

--- P4_2.cpp
+++ P4_2.cpp
@@ -7,51 +7,51 @@ int n,m;
 bool isExit(char c,int x,int y)
 {
     for(int i=0;i<m;i++)
-        {
-	        if(a[i].c == c && a[i].x == x && a[i].y == y)
-		            return 1;
-			        }
-				    return 0;
-				    }
+    {
+        if(a[i].c == c && a[i].x == x && a[i].y == y)
+            return 1;
+    }
+    return 0;
+}
 
-				    int main()
-				    {
-				        cin>>n;
-					    cin>>m;
-					        for(int i=0;i<m;i++)
-						    {
-						            cin>>a[i].c>>a[i].x>>a[i].y;
-							        }
-								    int ans = 0;
-								        /*
-									    使用循环判断是否有边长为n的正方形
-									        关系如下：假如(x, y)存在边长为p的正方形，则必然存在边： V x y, V x+1 y, ...V x+p-1 y; H x y, H x y+1,...H x y+p-1
-										    且，(x, y+p),(x+p, y)也存在类似上述关系。
-										        测试数据：
-											    3 7
-											        V 1 1 H 1 1 V 1 2 H 2 1 H 2 2 V 1 3 H 1 2
-												    ANS = 2
-												        */
-													    for(int l=1;l<n;l++)                     // 边长从1到n-1
-													        {
-														        for(int x=1;x<=n-l;x++)
-															        {
-																            for(int y=1;y<=n-l;y++)         //  可能范围内的每一点横坐标和纵坐标
-																	                {
-																			                int i;
-																					                for(i=0;i<l;i++)            // 从四个方向分别找四条边
-																							                {
-																									                    if(isExit('V', x+i, y) && isExit('H', x, y+i) && isExit('V', x+i, y+l) && isExit('H', x+l, y+i))
-																											                            continue;
-																														                        else
-																																	                        break;
-																																				                }
-																																						                if(i==l)                    // 四条边全部找到
-																																								                    ans++;
-																																										                }
-																																												        }
-																																													    }
-																																													        cout<<ans<<endl;
-																																														    return 0;
-																																														    }
-																																														    
+int main()
+{
+    cin>>n;
+    cin>>m;
+    for(int i=0;i<m;i++)
+    {
+        cin>>a[i].c>>a[i].x>>a[i].y;
+    }
+    int ans = 0;
+    /*
+    使用循环判断是否有边长为n的正方形
+    关系如下：假如(x, y)存在边长为p的正方形，则必然存在边： V x y, V x+1 y, ...V x+p-1 y; H x y, H x y+1,...H x y+p-1
+    (x, y+p),(x+p, y)也存在类似上述关系。
+    测试数据：
+    3 7
+    V 1 1 H 1 1 V 1 2 H 2 1 H 2 2 V 1 3 H 1 2
+    ANS = 2
+    */
+    for(int l=1;l<n;l++)                     // 边长从1到n-1
+    {
+        for(int x=1;x<=n-l;x++)
+        {
+            for(int y=1;y<=n-l;y++)         //  可能范围内的每一点横坐标和纵坐标
+            {
+                int i;
+                for(i=0;i<l;i++)            // 从四个方向分别找四条边
+                {
+                    if(isExit('V', x+i, y) && isExit('H', x, y+i) && isExit('V', x+i, y+l) && isExit('H', x+l, y+i))
+                        continue;
+                    else
+                        break;
+                }
+                if(i==l)                    // 四条边全部找到
+                    ans++;
+            }
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
+
